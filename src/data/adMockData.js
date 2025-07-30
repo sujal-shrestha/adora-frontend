229c39894afd2e8e132aeabec35bfecc17f528cf
sujal-shrestha <sujalwork69@gmail.com>
2025-07-30 14:51:14 +0545

    ➕ Add mock ad data for niche-based Facebook ads in SpyTool

---
+++ src/data/adMockData.js
@@ -0,0 +1,58 @@
+const mockAdData = {
+  Fitness: [
+    {
+      id: 1,
+      title: "Burn Fat Fast with 15-Min Workouts",
+      description: "Get in shape without a gym. Real results in weeks!",
+      brand: "FitNation",
+      image: "https://www.hearstmagazines.co.uk/media/catalog/product/cache/51953653fdef8ef18ec1474aa5cd7e22/1/5/15minweightloss_7.jpg",
+      link: "https://www.facebook.com/ads/library/?id=123456789"
+    },
+    {
+      id: 2,
+      title: "Home Fitness Plan for Busy Professionals",
+      description: "No equipment needed. Transform your body at home.",
+      brand: "CoreX",
+      image: "https://sfhealthtech.com/cdn/shop/articles/guy-training-home.jpg?v=1638186084",
+      link: "https://www.facebook.com/ads/library/?id=987654321"
+    }
+  ],
+  Skincare: [
+    {
+      id: 3,
+      title: "Glow Up with Natural Skincare",
+      description: "Vegan-friendly, dermatologist-approved glow kit.",
+      brand: "LushCare",
+      image: "https://cdn.dribbble.com/userupload/38635102/file/original-c1c44c3443731e6467f488e428821d9a.jpg?resize=400x0",
+      link: "https://www.facebook.com/ads/library/?id=2233445566"
+    },
+    {
+      id: 4,
+      title: "Erase Acne in 7 Days",
+      description: "Join thousands who cleared their skin with ClearZen.",
+      brand: "ClearZen",
+      image: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/b2892a73497731.5c0b2d9e3b61a.jpg",
+      link: "https://www.facebook.com/ads/library/?id=6655443322"
+    }
+  ],
+  Finance: [
+    {
+      id: 5,
+      title: "Start Investing with Just $5",
+      description: "No experience needed. Learn and grow your savings.",
+      brand: "WealthStep",
+      image: "https://www.creatopy.com/blog/wp-content/uploads/2019/09/house-insurance-hands-giving-house-439x600.jpg",
+      link: "https://www.facebook.com/ads/library/?id=9988776655"
+    },
+    {
+      id: 6,
+      title: "Get Funded in 24 Hours",
+      description: "Apply now and get up to $10K for your side hustle.",
+      brand: "FundFlash",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwpnspK6KduH6aOA-3Ma8l59OCi08u1FMknw&s",
+      link: "https://www.facebook.com/ads/library/?id=5544332211"
+    }
+  ]
+};
+
+export default mockAdData;
